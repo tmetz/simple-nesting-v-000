262cc14268f1bb69d0f7fdd320d71bbbe68742f2
Tammy Metz <tammy@tammymetz.com>
2018-08-02 06:30:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,6 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
 
 
 end
