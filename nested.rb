73360c85f00fb5eecd2f1a2d8d06b05bd7f81a75
Tammy Metz <tammy@tammymetz.com>
2018-08-02 06:49:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
 
-adding_matz
